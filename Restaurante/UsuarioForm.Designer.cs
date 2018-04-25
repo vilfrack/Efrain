31068d91d6bd3652e8340ab6a9986e3788c18f39
Juan villarroel <vilfrack@users.noreply.github.com>
2018-04-25 19:56:11 -0400

    CRUD usuario

---
+++ Restaurante/UsuarioForm.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Restaurante
+{
+    partial class UsuarioForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIDUsuario = new System.Windows.Forms.TextBox();
+            this.labelIDRol = new System.Windows.Forms.Label();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.btnEliminarUsuario = new System.Windows.Forms.Button();
+            this.btnCrearUsuario = new System.Windows.Forms.Button();
+            this.btnEditarUsuario = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.GridViewUsuario = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.GridViewRol = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewUsuario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewRol)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "ID Usuario";
+            // 
+            // txtIDUsuario
+            // 
+            this.txtIDUsuario.Enabled = false;
+            this.txtIDUsuario.Location = new System.Drawing.Point(91, 15);
+            this.txtIDUsuario.Name = "txtIDUsuario";
+            this.txtIDUsuario.Size = new System.Drawing.Size(182, 20);
+            this.txtIDUsuario.TabIndex = 54;
+            // 
+            // labelIDRol
+            // 
+            this.labelIDRol.AutoSize = true;
+            this.labelIDRol.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIDRol.Location = new System.Drawing.Point(84, 59);
+            this.labelIDRol.Name = "labelIDRol";
+            this.labelIDRol.Size = new System.Drawing.Size(0, 17);
+            this.labelIDRol.TabIndex = 53;
+            this.labelIDRol.Visible = false;
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Location = new System.Drawing.Point(280, 225);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(84, 51);
+            this.btnAtras.TabIndex = 52;
+            this.btnAtras.Text = "Atras";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarUsuario
+            // 
+            this.btnEliminarUsuario.Enabled = false;
+            this.btnEliminarUsuario.Location = new System.Drawing.Point(190, 225);
+            this.btnEliminarUsuario.Name = "btnEliminarUsuario";
+            this.btnEliminarUsuario.Size = new System.Drawing.Size(84, 51);
+            this.btnEliminarUsuario.TabIndex = 51;
+            this.btnEliminarUsuario.Text = "Eliminar";
+            this.btnEliminarUsuario.UseVisualStyleBackColor = true;
+            this.btnEliminarUsuario.Click += new System.EventHandler(this.btnEliminarUsuario_Click);
+            // 
+            // btnCrearUsuario
+            // 
+            this.btnCrearUsuario.Location = new System.Drawing.Point(10, 224);
+            this.btnCrearUsuario.Name = "btnCrearUsuario";
+            this.btnCrearUsuario.Size = new System.Drawing.Size(84, 51);
+            this.btnCrearUsuario.TabIndex = 50;
+            this.btnCrearUsuario.Text = "Crear";
+            this.btnCrearUsuario.UseVisualStyleBackColor = true;
+            this.btnCrearUsuario.Click += new System.EventHandler(this.btnCrearUsuario_Click);
+            // 
+            // btnEditarUsuario
+            // 
+            this.btnEditarUsuario.Enabled = false;
+            this.btnEditarUsuario.Location = new System.Drawing.Point(100, 224);
+            this.btnEditarUsuario.Name = "btnEditarUsuario";
+            this.btnEditarUsuario.Size = new System.Drawing.Size(84, 51);
+            this.btnEditarUsuario.TabIndex = 49;
+            this.btnEditarUsuario.Text = "Editar";
+            this.btnEditarUsuario.UseVisualStyleBackColor = true;
+            this.btnEditarUsuario.Click += new System.EventHandler(this.btnEditarUsuario_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(91, 42);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(182, 20);
+            this.txtUsuario.TabIndex = 47;
+            // 
+            // GridViewUsuario
+            // 
+            this.GridViewUsuario.AllowUserToAddRows = false;
+            this.GridViewUsuario.AllowUserToDeleteRows = false;
+            this.GridViewUsuario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridViewUsuario.Location = new System.Drawing.Point(12, 281);
+            this.GridViewUsuario.Name = "GridViewUsuario";
+            this.GridViewUsuario.Size = new System.Drawing.Size(618, 176);
+            this.GridViewUsuario.TabIndex = 46;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(90, 68);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(182, 20);
+            this.txtPassword.TabIndex = 56;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 59;
+            this.label4.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(91, 94);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(182, 20);
+            this.txtNombre.TabIndex = 58;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 17);
+            this.label5.TabIndex = 61;
+            this.label5.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(91, 120);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(182, 20);
+            this.txtApellido.TabIndex = 60;
+            // 
+            // GridViewRol
+            // 
+            this.GridViewRol.AllowUserToAddRows = false;
+            this.GridViewRol.AllowUserToDeleteRows = false;
+            this.GridViewRol.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridViewRol.Location = new System.Drawing.Point(6, 19);
+            this.GridViewRol.Name = "GridViewRol";
+            this.GridViewRol.Size = new System.Drawing.Size(338, 159);
+            this.GridViewRol.TabIndex = 64;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.GridViewRol);
+            this.groupBox1.Location = new System.Drawing.Point(280, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 191);
+            this.groupBox1.TabIndex = 65;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Roles";
+            // 
+            // UsuarioForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 469);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIDUsuario);
+            this.Controls.Add(this.labelIDRol);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.btnEliminarUsuario);
+            this.Controls.Add(this.btnCrearUsuario);
+            this.Controls.Add(this.btnEditarUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.GridViewUsuario);
+            this.Name = "UsuarioForm";
+            this.Text = "UsuarioForm";
+            this.Load += new System.EventHandler(this.UsuarioForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewUsuario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridViewRol)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIDUsuario;
+        private System.Windows.Forms.Label labelIDRol;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Button btnEliminarUsuario;
+        private System.Windows.Forms.Button btnCrearUsuario;
+        private System.Windows.Forms.Button btnEditarUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.DataGridView GridViewUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.DataGridView GridViewRol;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ Restaurante/UsuarioForm.cs
@@ -0,0 +1,97 @@
+﻿using Datos.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurante
+{
+    public partial class UsuarioForm : Form
+    {
+        BDRestauranteEntities EF = new BDRestauranteEntities();
+        Utilidades.Utilidades utilidades = new Utilidades.Utilidades();
+        Datos.EF.Usuario Usuario = new Usuario();
+        public int _IDUsuario = 0;
+        public UsuarioForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UsuarioForm_Load(object sender, EventArgs e)
+        {
+            utilidades.ConfiguracionFormulario(this);
+            utilidades.ConfiguracionGridview(GridViewRol);
+            utilidades.ConfiguracionGridview(GridViewUsuario);
+            BindGridRoles();
+        }
+        private void BindGridRoles()
+        {
+            GridViewRol.DataSource = EF.Rol.ToList();
+        }
+        private void BindGridRolesPorUsuarios()
+        {
+            GridViewRol.DataSource = EF.Rol.ToList();
+        }
+        private void BindGridRolesUsuarios()
+        {
+            GridViewUsuario.DataSource = EF.Rol.ToList();
+        }
+
+        private void btnCrearUsuario_Click(object sender, EventArgs e)
+        {
+            Usuario.Login = txtUsuario.Text;
+            Usuario.Nombre = txtNombre.Text;
+            Usuario.Apellido = txtApellido.Text;
+            Usuario.Password = txtPassword.Text;
+            EF.Usuario.Add(Usuario);
+            EF.SaveChanges();
+            BindGridRolesUsuarios();
+            MessageBox.Show("Usuario agregado");
+        }
+
+        private void btnEditarUsuario_Click(object sender, EventArgs e)
+        {
+            if (txtIDUsuario.Text != "")
+            {
+                int IDUsuario = Convert.ToInt32(txtIDUsuario.Text);
+                Usuario usuario = EF.Usuario.Find(IDUsuario);
+                if (usuario != null)
+                {
+                    Usuario.Login = txtUsuario.Text;
+                    Usuario.Nombre = txtNombre.Text;
+                    Usuario.Apellido = txtApellido.Text;
+                    Usuario.Password = txtPassword.Text;
+                    //usuario.IDUsuario = Convert.ToInt32(txtIDUsuario.Text);
+                    EF.SaveChanges();
+                    BindGridRolesUsuarios();
+                    MessageBox.Show("Regitro modificado");
+                }
+            }
+        }
+
+        private void btnEliminarUsuario_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Seguro desea eliminar el registro?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (txtIDUsuario.Text != "")
+                {
+                    int IDUsuario = Convert.ToInt32(txtIDUsuario.Text);
+                    Usuario usuario = EF.Usuario.Find(IDUsuario);
+                    if (usuario != null)
+                    {
+                        EF.Usuario.Remove(usuario);
+                        EF.SaveChanges();
+                        BindGridRolesUsuarios();
+                        MessageBox.Show("Regitro eliminado");
+                    }
+
+                }
+            }
+        }
+    }
+}
